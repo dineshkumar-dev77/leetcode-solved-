c370a9a788b49e84855e268d09c79f57cf6c418d
Dinesh kumar <dineshumar07@gmail.com>
2026-08-10 16:12:06 +0530

    Time: 36 ms (42.68%) | Memory: 137.3 MB (64.25%) - LeetSync

--- 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
+++ 3584-find-the-lexicographically-smallest-valid-sequence/find-the-lexicographically-smallest-valid-sequence.java
@@ -1,105 +1,32 @@
-import java.util.*;
-
 class Solution {
+  public int[] validSequence(String word1, String word2) {
+    int[] ans = new int[word2.length()];
+    // last[j] := the index i of the last occurrence in word1, where
+    // word1[i] == word2[j]
+    int[] last = new int[word2.length()];
+    Arrays.fill(last, -1);
 
-    public int[] validSequence(String word1, String word2) {
-
-        int n = word1.length();
-        int m = word2.length();
-
-    
-
-        int[][] next = new int[26][n + 1];
-
-        for (int c = 0; c < 26; c++) {
-            next[c][n] = -1;
-        }
-
-        for (int i = n - 1; i >= 0; i--) {
-
-            for (int c = 0; c < 26; c++) {
-                next[c][i] = next[c][i + 1];
-            }
-
-            next[word1.charAt(i) - 'a'][i] = i;
-        }
-
-       
-
-        int[] exact = new int[m + 1];
-
-        exact[m] = n;
-
-        int pos = n - 1;
-
-        for (int j = m - 1; j >= 0; j--) {
-
-            while (pos >= 0 &&
-                   word1.charAt(pos) != word2.charAt(j)) {
-                pos--;
-            }
-
-            if (pos < 0) {
-                exact[j] = -1;
-            } else {
-                exact[j] = pos;
-                pos--;
-            }
-        }
-
-        
-        int[] answer = new int[m];
-
-        int current = 0;
-        boolean changed = false;
-
-        for (int j = 0; j < m; j++) {
-
-            char target = word2.charAt(j);
-
-            int chosen = -1;
-
-           
-            for (int i = current; i < n; i++) {
-
-                boolean same = word1.charAt(i) == target;
-
-                if (same) {
-                    chosen = i;
-                    break;
-                }
-
-                if (!changed) {
-
-                   
-                    int remaining = j + 1;
-
-                    if (remaining == m) {
-                        chosen = i;
-                        changed = true;
-                        break;
-                    }
-
-                    if (exact[remaining] > i) {
-                        chosen = i;
-                        changed = true;
-                        break;
-                    }
-                }
-            }
-
-            if (chosen == -1) {
-                return new int[0];
-            }
-
-            if (word1.charAt(chosen) != target) {
-                changed = true;
-            }
-
-            answer[j] = chosen;
-            current = chosen + 1;
-        }
-
-        return answer;
+    int i = word1.length() - 1;
+    int j = word2.length() - 1;
+    while (i >= 0 && j >= 0) {
+      if (word1.charAt(i) == word2.charAt(j))
+        last[j--] = i;
+      --i;
     }
+
+    boolean canSkip = true;
+    j = 0;
+    for (i = 0; i < word1.length(); ++i) {
+      if (j == word2.length())
+        break;
+      if (word1.charAt(i) == word2.charAt(j)) {
+        ans[j++] = i;
+      } else if (canSkip && (j == word2.length() - 1 || i < last[j + 1])) {
+        canSkip = false;
+        ans[j++] = i;
+      }
+    }
+
+    return j == word2.length() ? ans : new int[0];
+  }
 }
