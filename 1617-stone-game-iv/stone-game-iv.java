0f679af27136fe02b9fbeedc7b4f5031cc3ca997
Dinesh kumar <dineshumar07@gmail.com>
2026-08-10 22:15:06 +0530

    Time: 14 ms (91.38%) | Memory: 42.4 MB (78.33%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -1,15 +1,26 @@
 class Solution {
-  public boolean winnerSquareGame(int n) {
-    // dp[i] := the winning result for n = i
-    boolean[] dp = new boolean[n + 1];
 
-    for (int i = 1; i <= n; ++i)
-      for (int j = 1; j * j <= i; ++j)
-        if (!dp[i - j * j]) { // Removing j^2 stones make the opponent lose.
-          dp[i] = true;       // So, we win.
-          break;
+    public boolean winnerSquareGame(int n) {
+
+        boolean[] dp = new boolean[n + 1];
+
+        // dp[i] = true means the player whose turn it is
+        // can win when there are i stones.
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j * j <= i; j++) {
+
+                // Remove j*j stones.
+                // If the opponent is in a losing position,
+                // the current player wins.
+                if (!dp[i - j * j]) {
+                    dp[i] = true;
+                    break;
+                }
+            }
         }
 
-    return dp[n];
-  }
+        return dp[n];
+    }
 }
