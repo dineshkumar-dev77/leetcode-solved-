b5201cbe5ac9ce31dd5453ad41ae5f8c5338c96a
Dinesh kumar <dineshumar07@gmail.com>
2026-08-24 21:04:23 +0530

    Time: 4 ms (98.09%) | Memory: 91.2 MB (45.92%) - LeetSync

---
+++ 396-rotate-function/rotate-function.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxRotateFunction(int[] nums) {
+
+        int n = nums.length;
+
+        long sum = 0;
+        long f = 0;
+
+        // Calculate sum and F(0)
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            f += (long) i * nums[i];
+        }
+
+        long max = f;
+
+        // Calculate F(1), F(2), ... F(n-1)
+        for (int k = 1; k < n; k++) {
+
+            f = f + sum - (long) n * nums[n - k];
+
+            max = Math.max(max, f);
+        }
+
+        return (int) max;
+    }
+}
